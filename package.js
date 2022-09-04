640117f7c197eb9412cf644d06bd50ce535eea35
Aram Kocharyan <akarmenia@gmail.com>
2022-09-04 09:26:34 -0700

    Meteor v2 upgrade

--- package.js
+++ package.js
@@ -1,22 +1,22 @@
 Package.describe({
   name: 'aramk:checkbox',
   summary: 'A reactive checkbox widget',
-  version: '1.1.0',
+  version: '2.0.0',
   git: 'https://github.com/aramk/meteor-checkbox.git'
 });
 
-Package.on_use(function(api) {
+Package.onUse(function(api) {
   api.versionsFrom('METEOR@1.6.1');
   api.use([
     'templating@1.3.2',
     'underscore',
     'jquery',
-    'urbanetic:utility@2.0.0'
+    'urbanetic:utility@3.0.0'
   ], 'client');
   api.use([
     'aramk:semantic-ui@2.4.1_1'
   ], 'client', {weak: true});
-  api.add_files([
+  api.addFiles([
     'src/checkbox.html',
     'src/checkbox.js'
   ], 'client');
